b670b2ed3ad23f50bd26dbb0c7d6125017414809
DwayneDevelops <DwayneDevelops@gmail.com>
2019-05-28 15:08:46 -0500

    building out post request

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,6 +1,28 @@
-const Posts = require('./posts-router.js');
+const Posts = require('../data/db.js');
 
 const router = require('express').Router();
+
+router.post('/', async (req, res) => {
+    const {title, contents} = req.body;
+    
+    if ( title && contents ){
+        try {
+            const post = await Posts.insert(req.body)
+            res.status(201).json(post);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ 
+                message: 'Error retrieving post'
+            });
+        }
+    } else {
+        res.status(404).json({ message: 'Please fill in Title and Contents and try again'});
+    }
+});
+
+router.post('/:id/comments', async (req, res) => {
+    
+})
 
 router.get('/', async (req, res) => {
     try {
@@ -10,6 +32,23 @@ router.get('/', async (req, res) => {
         console.log(error);
         res.status(500).json({
             message: 'Error retrieving the posts'
+        });
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.findById(req.params.id);
+
+        if (post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: 'Could not find the post with the specified ID'});
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving the post'
         });
     }
 });
